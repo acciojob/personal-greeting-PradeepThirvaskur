8a56def80ab528b84588f4d4e034e3aa645629b5
pradeep thirvaskur <142670238+PradeepThirvaskur@users.noreply.github.com>
2024-06-26 18:02:11 +0000

    solved

--- src/components/App.js
+++ src/components/App.js
@@ -1,13 +1,21 @@
 
 import React from "react";
+import { useState } from "react";
 import './../styles/App.css';
 
 const App = () => {
+  const [name,setName]=useState("");
+  function onChange(event){
+    setName(event.target.value);
+  }
   return (
     <div>
-        {/* Do not remove the main div */}
+       <input  value={name} onChange={onChange}>
+       </input>
+       {name ?  <p>Hello {name}!</p> :  null } 
+       
     </div>
-  )
+  );
 }
 
 export default App
